f37a2949f604ea175b54313c0600f1a4557a3ee2
AnotherAcorn <gnarlyseacucumber@gmail.com>
2021-02-06 21:57:04 +0000

    Got it to give p2's name instead of "Player" and a string of numbers :D

--- App.java
+++ App.java
@@ -18,51 +18,54 @@ public class App {
         noWinner = false;
 //create users and their names
         System.out.println("Enter player 1's name:");
-        p1 = input.nextLine();
+        Player p1 = new Player(input.nextLine());
         System.out.println("Enter player 2's name:");
-        p2 = input.nextLine();
-        System.out.println("Player 1: " + p1 + "\nPlayer 2: " + p2);
+        Player p2 = new Player(input.nextLine());
+        System.out.println("Player 1: " + p1.getName() + "\nPlayer 2: " + p2.getName());
 //start while loop and game 
       while (noWinner == false)
       {
+        Board game = new Board();
 //player 1 plays
-        System.out.println("\nGive " + p1 + "'s next move:");
+        System.out.println("\nGive " + p1.getName() + "'s next move:");
         move1 = input.nextLine();
-        moveItt(move1);
-        printBoard();
+        game.moveIt(move1, "x");
+        game.printBoard();
 //print a cute border to signal end of turn
-    for (int i = 1; i < 51; i++)
-    {
-      System.out.print("*");
-    }
+    printBorder();
 //Checks for Player 1 having won and acts accordingly if true
-        Winner1();
-        if (win1 == true)
-        {
-          System.out.println(p1 + " has won!");
-          noWinner = true;
-          break;
-        }
+        // Winner1();
+        // if (win1 == true)
+        // {
+        //   System.out.println(p1 + " has won!");
+        //   noWinner = true;
+        //   break;
+        // }
 //player 2 plays
-        System.out.println("\n Give " + p2 + "'s next move:");
+        System.out.println("\n Give " + p2.getName() + "'s next move:");
         move2 = input.nextLine();
-        moveIt(move2);
-        printBoard();
+        game.moveIt(move2, "o");
+        game.printBoard();
 //print a cute border to signal end of turn
-    for (int i = 1; i < 51; i++)
-    {
-      System.out.print("*");
-    }
+        printBorder();
 //Player 2 win sequence
-        Winner2();
-        if (win2 == true)
-        {
-          System.out.println(p2 + " has won!");
-          noWinner = true;
-          break;
+        // Winner2();
+        // if (win2 == true)
+        // {
+        //   System.out.println(p2.getName() + " has won!");
+        //   noWinner = true;
+        //   break;
         }
         //Winner1(win1, A1, A2, A3, B1, B2, B3, C1, C2, C3);  
     }
+
+  //How to print a border 
+  public static void printBorder()
+  {
+    for (int i = 1; i < 51; i++)
+      {
+        System.out.print ("*");
+      }
   }
-  
 }
+
--- Player.java
+++ Player.java
@@ -1,20 +1,18 @@
 import java.util.Scanner;
 public class Player {
+  private String name;
+  //a constructor is a method without a return type that shares the class's name
+  public Player(String name)
+  {
+    this.name = name;
+  }
+  public void setName(String name)
+  {
+    this.name = name;
+  }
 
-public void setPlayer1(String player1)
-{
-  player1 = p1;
-}
-public void setPlayer2(String player2)
-{
-  player2 = p2;
-}
-public String getPlayer1(String player1)
-{
-  return player1;
-}
-public String getPlayer2(String player2)
-{
-  return player2;
-}
+  public String getName()
+  {
+    return name;
+  }
 }
